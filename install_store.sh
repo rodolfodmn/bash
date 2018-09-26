d82336f37576d4b1b6d1d6119b4466f8d4f41749
Rodolfo Domingos <rodolfo.domingos@bistwobis.com.br>
2018-09-26 16:54:17 -0300

    adição do parametro para instalação da loja gitlab

--- install_store.sh
+++ install_store.sh
@@ -7,6 +7,9 @@ elif [ -n "$1" ]; then
     if [ "$1" == 'b' ]; then
         echo 'install basic store script :)'
         php ~/dev/php_bash/storeBasicInstall.php
+    elif [ "$1" == 'l' ]; then
+        echo 'install gitlab store script :)'
+        php ~/dev/php_bash/storeLabInstall.php
     else
         echo "seu parametro nao deve estar incorreto :("
     fi
