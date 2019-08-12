3eae6b074bb3e329a147bf0aec9b5ccd215229a6
Rodolfo Domingos <rodolfo.domingos@bistwobis.com.br>
2019-08-12 15:31:10 -0300

    bashs para instalacao de modulos

--- git_commit_api_places.sh
+++ git_commit_api_places.sh
@@ -5,23 +5,23 @@ elif [ -n "$1" ]; then
     if [ "$1" == 'i' ]; then
         echo 'instalando marketplace shipping/payment :)'
         # Marketplace Shipping
-        cp -r ~/dev/modulos/marketplace-shipping/*  .
+        cp -r ~/dev/modulos/modulo-gubee/marketplace-shipping/*  .
         git add .
         git commit -m "Instalacao - Marketplace Shipping - #$2"
         
         # Marketplace Payment        
-        cp -r ~/dev/modulos/marketplace-payment/*  .
+        cp -r ~/dev/modulos/modulo-gubee/marketplace-payment/*  .
         git add .
         git commit -m "Instalacao - Marketplace Payment - #$2"
     elif [ "$1" == 'a' ]; then
         echo 'Atualizando marketplace shipping/payment :)'
         # Marketplace Shipping
-        cp -r ~/dev/modulos/marketplace-shipping/*  .
+        cp -r ~/dev/modulos/modulo-gubee/marketplace-shipping/*  .
         git add .
         git commit -m "Atualizacao - Marketplace Shipping - #$2"
         
         # Marketplace Payment        
-        cp -r ~/dev/modulos/marketplace-payment/*  .
+        cp -r ~/dev/modulos/modulo-gubee/marketplace-payment/*  .
         git add .
         git commit -m "Atualizacao - Marketplace Payment - #$2"
     else
